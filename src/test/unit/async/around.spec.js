5097df7955dc7f2cd4801e00689d7d2d378a1728
Matthew Adams <matthew@matthewadams.me>
2019-04-02 15:08:26 -0500

    async advice tested

---
+++ src/test/unit/async/around.spec.js
@@ -0,0 +1,176 @@
+/* global it, describe, beforeEach, before */
+'use strict'
+
+const chai = require('chai')
+const expect = chai.expect
+chai.use(require('dirty-chai'))
+
+const { AsyncAround } = require('../../../main/Advice')
+
+const pause = require('./pause')
+const methodDelay = 1
+const adviceDelay = methodDelay + 1
+
+class Counter {
+  constructor (value = 0) {
+    this._value = value
+    this.gets = 0
+  }
+
+  async increment (step = 1) {
+    await pause(methodDelay)
+    this._value += step
+  }
+
+  async setValue (value) {
+    await pause(methodDelay)
+    this._value = value
+  }
+
+  async getValue () {
+    await pause(methodDelay)
+    this.gets++
+    return this._value
+  }
+}
+
+class Cache {
+  constructor () {
+    this._values = {}
+  }
+
+  async set (key, value) {
+    await pause(methodDelay)
+    this._values[key] = value
+  }
+
+  async get (key) {
+    await pause(methodDelay)
+    return this._values[key]
+  }
+
+  async has (key) {
+    await pause(methodDelay)
+    return key in this._values
+  }
+}
+
+class CacheAdvice {
+  constructor () {
+    this._cache = new Cache()
+    this.advise = this.advise.bind(this)
+  }
+
+  async advise (thisJoinPoint, value) {
+    await pause(adviceDelay)
+
+    if (thisJoinPoint.set) return thisJoinPoint.proceed()
+
+    if (!(await this._cache.has(thisJoinPoint.fullName))) {
+      await this._cache.set(thisJoinPoint.fullName, value || (await thisJoinPoint.proceed()))
+    }
+
+    return this._cache.get(thisJoinPoint.fullName)
+  }
+}
+
+describe('unit tests of asynchronous around advice', function () {
+  before(function () {
+    expect(adviceDelay).to.be.above(methodDelay)
+  })
+
+  describe('Counter', function () {
+    it('should work', async function () {
+      const counter = new Counter()
+
+      expect(await counter.getValue()).to.equal(0)
+      await counter.increment()
+      expect(await counter.getValue()).to.equal(1)
+      await counter.increment(1)
+      expect(await counter.getValue()).to.equal(2)
+      await counter.increment(-1)
+      expect(await counter.getValue()).to.equal(1)
+    })
+  })
+
+  describe('parameterless around advice', function () {
+    let counter = new Counter()
+    let testCounter
+
+    beforeEach(function () {
+      const advice = new CacheAdvice()
+
+      const Memoize = AsyncAround(advice.advise) // note that advise is bind()'ed!
+
+      class TestCounter extends Counter {
+        @Memoize
+        async getValue () {
+          return super.getValue()
+        }
+      }
+
+      testCounter = new TestCounter()
+    })
+
+    it('should work', async function () {
+      expect(await counter.getValue()).to.equal(0)
+      await counter.increment()
+      expect(await counter.getValue()).to.equal(1)
+      await counter.increment(1)
+      expect(await counter.getValue()).to.equal(2)
+
+      expect(testCounter.gets).to.equal(0)
+      expect(await testCounter.getValue()).to.equal(0)
+      expect(testCounter.gets).to.equal(1)
+
+      await testCounter.increment()
+      expect(await testCounter.getValue()).to.equal(0)
+      expect(testCounter.gets).to.equal(1)
+
+      await testCounter.increment(1)
+      expect(await testCounter.getValue()).to.equal(0)
+      expect(testCounter.gets).to.equal(1)
+    })
+  })
+
+  describe('parameterized around advice', async function () {
+    let counter = new Counter()
+    let testCounter
+    let aValue = 42
+
+    beforeEach(function () {
+      const advice = new CacheAdvice()
+
+      const JustReturn = value => AsyncAround(async thisJoinPoint => advice.advise(thisJoinPoint, value))
+
+      class TestCounter extends Counter {
+        @JustReturn(aValue)
+        async getValue () {
+          return super.getValue()
+        }
+      }
+
+      testCounter = new TestCounter()
+    })
+
+    it('should work', async function () {
+      expect(await counter.getValue()).to.equal(0)
+      await counter.increment()
+      expect(await counter.getValue()).to.equal(1)
+      await counter.increment(1)
+      expect(await counter.getValue()).to.equal(2)
+
+      expect(testCounter.gets).to.equal(0)
+      expect(await testCounter.getValue()).to.equal(aValue)
+      expect(testCounter.gets).to.equal(0)
+
+      await testCounter.increment()
+      expect(await testCounter.getValue()).to.equal(aValue)
+      expect(testCounter.gets).to.equal(0)
+
+      await testCounter.increment(1)
+      expect(await testCounter.getValue()).to.equal(aValue)
+      expect(testCounter.gets).to.equal(0)
+    })
+  })
+})
---
+++ src/test/unit/async/non-around.spec.js
@@ -0,0 +1,330 @@
+/* global it, describe, before */
+'use strict'
+
+const chai = require('chai')
+const expect = chai.expect
+const fail = expect.fail
+chai.use(require('dirty-chai'))
+
+const { AsyncBefore, AsyncAfterReturning, AsyncAfterThrowing, AsyncAfterFinally } = require('../../../main/Advice')
+
+const pause = require('./pause')
+
+const assertThisJoinPoint = thisJoinPoint => {
+  expect(thisJoinPoint).to.be.ok()
+  expect(thisJoinPoint.clazz).to.be.ok()
+  expect(thisJoinPoint.name).to.be.ok()
+  expect(thisJoinPoint.descriptors).to.be.ok()
+  expect(thisJoinPoint.descriptors.original).to.be.ok()
+  expect(thisJoinPoint.descriptors.advised).to.be.ok()
+  expect(thisJoinPoint.thiz).to.be.ok()
+  expect(thisJoinPoint.args).to.be.ok()
+  expect(thisJoinPoint.fullName).to.be.ok()
+}
+
+describe('unit tests of asynchronous', function () {
+  const methodDelay = 1
+  const adviceDelay = methodDelay + 5
+  const val = 1
+  const err = 'boom'
+
+  before(function () {
+    expect(adviceDelay).to.be.above(methodDelay)
+  })
+
+  describe('parameterless advice', function () {
+    it('should work with AsyncBefore', async function () {
+      let count = 0
+
+      const ParameterlessBeforeCount = AsyncBefore(async thisJoinPoint => {
+        assertThisJoinPoint(thisJoinPoint)
+        await pause(adviceDelay)
+        count++
+      })
+
+      class Class {
+        @ParameterlessBeforeCount
+        async doesNotThrow (delayMillis, value) {
+          await pause(delayMillis)
+          return value
+        }
+        @ParameterlessBeforeCount
+        async doesThrow (delayMillis, value) {
+          await pause(delayMillis)
+          throw new Error(err)
+        }
+      }
+
+      const c = new Class()
+      const v = await c.doesNotThrow(methodDelay, val)
+      expect(count).to.equal(1)
+      expect(v).to.equal(val)
+
+      try {
+        await c.doesThrow(methodDelay, val)
+        fail('c.doesThrow() should have thrown')
+      } catch (e) {
+        expect(count).to.equal(2)
+        expect(e.message).to.equal(err)
+      }
+    })
+
+    it('should work with AsyncAfterReturning', async function () {
+      let count = 0
+
+      const ParameterlessAfterReturningCount = AsyncAfterReturning(async function ({ thisJoinPoint, returnValue }) {
+        assertThisJoinPoint(thisJoinPoint)
+        expect('returnValue' in arguments[0])
+        await pause(adviceDelay)
+        count++
+      })
+
+      class Class {
+        @ParameterlessAfterReturningCount
+        async doesNotThrow (delayMillis, value) {
+          await pause(delayMillis)
+          return value
+        }
+        @ParameterlessAfterReturningCount
+        async doesThrow (delayMillis, value) {
+          await pause(delayMillis)
+          throw new Error(err)
+        }
+      }
+
+      const c = new Class()
+      const v = await c.doesNotThrow(methodDelay, val)
+      expect(count).to.equal(1)
+      expect(v).to.equal(val)
+
+      try {
+        await c.doesThrow(methodDelay, val)
+        fail('c.doesThrow() should have thrown')
+      } catch (e) {
+        expect(count).to.equal(1)
+        expect(e.message).to.equal(err)
+      }
+    })
+
+    it('should work with AsyncAfterThrowing', async function () {
+      let count = 0
+
+      const ParameterlessAfterThrowingCount = AsyncAfterThrowing(async function ({ thisJoinPoint, error }) {
+        assertThisJoinPoint(thisJoinPoint)
+        expect('error' in arguments[0])
+        await pause(adviceDelay)
+        count++
+      })
+
+      class Class {
+        @ParameterlessAfterThrowingCount
+        async doesNotThrow (delayMillis, value) {
+          await pause(delayMillis)
+          return value
+        }
+        @ParameterlessAfterThrowingCount
+        async doesThrow (delayMillis, value) {
+          await pause(delayMillis)
+          throw new Error(err)
+        }
+      }
+
+      const c = new Class()
+      const v = await c.doesNotThrow(methodDelay, val)
+      expect(count).to.equal(0)
+      expect(v).to.equal(val)
+
+      try {
+        await c.doesThrow(methodDelay, val)
+        fail('c.doesThrow() should have thrown')
+      } catch (e) {
+        expect(count).to.equal(1)
+        expect(e.message).to.equal(err)
+      }
+    })
+
+    it('should work with AsyncAfterFinally', async function () {
+      let count = 0
+
+      const ParameterlessAfterFinallyCount = AsyncAfterFinally(async ({ thisJoinPoint }) => {
+        assertThisJoinPoint(thisJoinPoint)
+        await pause(adviceDelay)
+        count++
+      })
+
+      class Class {
+        @ParameterlessAfterFinallyCount
+        async doesNotThrow (delayMillis, value) {
+          await pause(delayMillis)
+          return value
+        }
+        @ParameterlessAfterFinallyCount
+        async doesThrow (delayMillis, value) {
+          await pause(delayMillis)
+          throw new Error(err)
+        }
+      }
+
+      const c = new Class()
+      const v = await c.doesNotThrow(methodDelay, val)
+      expect(count).to.equal(1)
+      expect(v).to.equal(val)
+
+      try {
+        await c.doesThrow(methodDelay, val)
+        fail('c.doesThrow() should have thrown')
+      } catch (e) {
+        expect(count).to.equal(2)
+        expect(e.message).to.equal(err)
+      }
+    })
+  })
+
+  describe('parameterized', function () {
+    it('should work with AsyncBefore advice', async function () {
+      let count = 0
+
+      const ParameterizedBeforeCount = (d = 0) => AsyncBefore(async thisJoinPoint => {
+        assertThisJoinPoint(thisJoinPoint)
+        await pause(d)
+        count++
+      })
+
+      class Class {
+        @ParameterizedBeforeCount(adviceDelay)
+        async doesNotThrow (delayMillis, value) {
+          await pause(delayMillis)
+          return value
+        }
+        @ParameterizedBeforeCount(adviceDelay)
+        async doesThrow (delayMillis, value) {
+          await pause(delayMillis)
+          throw new Error(err)
+        }
+      }
+
+      const c = new Class()
+      const v = await c.doesNotThrow(methodDelay, val)
+      expect(count).to.equal(1)
+      expect(v).to.equal(val)
+
+      try {
+        await c.doesThrow(methodDelay, val)
+        fail('c.doesThrow() should have thrown')
+      } catch (e) {
+        expect(count).to.equal(2)
+        expect(e.message).to.equal(err)
+      }
+    })
+
+    it('should work with AsyncAfterReturning advice', async function () {
+      let count = 0
+
+      const ParameterizedAfterReturningCount = (d = 0) => AsyncAfterReturning(async function ({ thisJoinPoint, returnValue }) {
+        assertThisJoinPoint(thisJoinPoint)
+        expect('returnValue' in arguments[0])
+        await pause(d)
+        count++
+      })
+
+      class Class {
+        @ParameterizedAfterReturningCount(adviceDelay)
+        async doesNotThrow (delayMillis, value) {
+          await pause(delayMillis)
+          return value
+        }
+        @ParameterizedAfterReturningCount(adviceDelay)
+        async doesThrow (delayMillis, value) {
+          await pause(delayMillis)
+          throw new Error(err)
+        }
+      }
+
+      const c = new Class()
+      const v = await c.doesNotThrow(methodDelay, val)
+      expect(count).to.equal(1)
+      expect(v).to.equal(val)
+
+      try {
+        await c.doesThrow(methodDelay, val)
+        fail('c.doesThrow() should have thrown')
+      } catch (e) {
+        expect(count).to.equal(1)
+        expect(e.message).to.equal(err)
+      }
+    })
+
+    it('should work with AsyncAfterThrowing advice', async function () {
+      let count = 0
+
+      const ParameterizedAfterThrowingCount = (d = 0) => AsyncAfterThrowing(async function ({ thisJoinPoint, error }) {
+        assertThisJoinPoint(thisJoinPoint)
+        expect('error' in arguments[0])
+        await pause(d)
+        count++
+      })
+
+      class Class {
+        @ParameterizedAfterThrowingCount(adviceDelay)
+        async doesNotThrow (delayMillis, value) {
+          await pause(delayMillis)
+          return value
+        }
+        @ParameterizedAfterThrowingCount(adviceDelay)
+        async doesThrow (delayMillis, value) {
+          await pause(delayMillis)
+          throw new Error(err)
+        }
+      }
+
+      const c = new Class()
+      const v = await c.doesNotThrow(methodDelay, val)
+      expect(count).to.equal(0)
+      expect(v).to.equal(val)
+
+      try {
+        await c.doesThrow(methodDelay, val)
+        fail('c.doesThrow() should have thrown')
+      } catch (e) {
+        expect(count).to.equal(1)
+        expect(e.message).to.equal(err)
+      }
+    })
+
+    it('should work with AsyncAfterFinally advice', async function () {
+      let count = 0
+
+      const ParameterizedAfterFinallyCount = (d = 0) => AsyncAfterFinally(async function ({ thisJoinPoint }) {
+        assertThisJoinPoint(thisJoinPoint)
+        await pause(d)
+        count++
+      })
+
+      class Class {
+        @ParameterizedAfterFinallyCount(adviceDelay)
+        async doesNotThrow (delayMillis, value) {
+          await pause(delayMillis)
+          return value
+        }
+        @ParameterizedAfterFinallyCount(adviceDelay)
+        async doesThrow (delayMillis, value) {
+          await pause(delayMillis)
+          throw new Error(err)
+        }
+      }
+
+      const c = new Class()
+      const v = await c.doesNotThrow(methodDelay, val)
+      expect(count).to.equal(1)
+      expect(v).to.equal(val)
+
+      try {
+        await c.doesThrow(methodDelay, val)
+        fail('c.doesThrow() should have thrown')
+      } catch (e) {
+        expect(count).to.equal(2)
+        expect(e.message).to.equal(err)
+      }
+    })
+  })
+})
